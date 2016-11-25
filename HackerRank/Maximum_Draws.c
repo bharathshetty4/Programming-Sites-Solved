228c85c077ce7893d942e52ceb7e7e794ca13852
Bharath kumar <shettybharath4@gmail.com>
2016-11-25 05:20:08 +0000

    Checking credential-store

---
+++ HackerRank/Maximum_Draws.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+int a,b,i;
+scanf("%d",&a);
+for(i=0;i<a;i++){
+    scanf("%d",&b);     
+    printf("%d\n",b+1);
+       
+    }
+return 0;
+}
