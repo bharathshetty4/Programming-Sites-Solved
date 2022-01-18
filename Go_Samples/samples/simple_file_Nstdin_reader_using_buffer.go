5df2f83c48896ec848c722fc46b94fdaf33ebd88
Bharath Kumar <shettybharath4@gmail.com>
2022-01-18 20:25:48 +0530

    Update and rename simple_file_reader_using_buffer.go to simple_file_Nstdin_reader_using_buffer.go

--- Go_Samples/samples/simple_file_Nstdin_reader_using_buffer.go
+++ Go_Samples/samples/simple_file_Nstdin_reader_using_buffer.go
@@ -31,3 +31,22 @@ func main() {
 		fmt.Print(string(b[0:n]))
 	}
 }
+
+func stdinReader() {
+  reader := bufio.NewReader(os.Stdin)
+  fmt.Println("Simple Shell")
+  fmt.Println("---------------------")
+
+  for {
+    fmt.Print("-> ")
+    text, _ := reader.ReadString('\n')
+    // convert CRLF to LF
+    text = strings.Replace(text, "\n", "", -1)
+
+    if strings.Compare("hi", text) == 0 {
+      fmt.Println("hello, Yourself")
+      }
+    }	
+	
+	
+}
