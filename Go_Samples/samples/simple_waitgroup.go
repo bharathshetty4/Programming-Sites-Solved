bcaa0522f59632bdeb02da2794f0735e3fe0ff25
Bharath Kumar <shettybharath4@gmail.com>
2022-01-18 22:28:13 +0530

    Create simple_waitgroup.go

---
+++ Go_Samples/samples/simple_waitgroup.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func runner1(wg *sync.WaitGroup) {
+	defer wg.Done() // This decreases counter by 1
+	fmt.Print("\nI am first runner")
+
+}
+
+func runner2(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Print("\nI am second runner")
+}
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go runner1(&wg)
+	go runner2(&wg)
+	wg.Wait()
+}
