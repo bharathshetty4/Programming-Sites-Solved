a0be18493177c51e945bdf61e1160daa2dd4c7a8
Bharath Kumar <shettybharath4@gmail.com>
2022-01-19 21:20:44 +0530

    Update simple_channel.go

--- Go_Samples/samples/simple_channel.go
+++ Go_Samples/samples/simple_channel.go
@@ -7,6 +7,24 @@ import (
 	"io/ioutil"
 )
 
+type chanStruct struct {
+	intVal int
+	strVal string
+}
+
+func main() {
+	ch := make(chan chanStruct)
+	go updateChannel(ch)
+	recvdVal := <-ch
+	fmt.Println("Hello", recvdVal)
+}
+
+func updateChannel(ch chan chanStruct) {
+	obj := chanStruct{intVal: 4, strVal: "bks"}
+	ch <- obj
+}
+
+// another Example
 func MakeRequest(url string, ch chan<-string) {
 	start := time.Now()
 	resp, _ := http.Get(url)
@@ -16,7 +34,7 @@ func MakeRequest(url string, ch chan<-string) {
 	ch <- fmt.Sprintf("%.2f elapsed with response length: %d %s", secs, len(body), url)
 }
 
-func main() {
+func main1() {
 	start := time.Now()
 	ch := make(chan string)
 	urls := []string{"https://google.com","https://quora.com"}
