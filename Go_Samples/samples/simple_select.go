b849821d919d913e3e423cd5ae9da8f55b8c8eab
Bharath Kumar <shettybharath4@gmail.com>
2022-01-24 19:32:18 +0530

    Update simple_select.go

--- Go_Samples/samples/simple_select.go
+++ Go_Samples/samples/simple_select.go
@@ -26,7 +26,11 @@ func main() {
 	// function 2 in goroutine
 	go portal1(R1)
 	go portal2(R2)
-
+	i := 0
+	// Go’s select lets you wait on multiple channel operations.
+	// can have 'default:' case or timer inside.
+	// NOTE: If a select statement has non-blocking case like default or timer, it can choose any one of those and here
+	// in this case we may never hit the timer at all
 	select {
 
 	// case 1 for portal 1
@@ -36,6 +40,15 @@ func main() {
 	// case 2 for portal 2
 	case op2 := <-R2:
 		fmt.Println(op2)
+
+	case <-time.After(time.Nanosecond):
+		fmt.Printf("\nTimeout\n")
+		return
+	default:
+		if i == 2 {
+			break
+		}
+		fmt.Println("def")
 	}
 
 }
