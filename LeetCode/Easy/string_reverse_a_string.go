779a1f2a5af3283199047ea456aa0f18ba204fe1
Bharath Kumar <shettybharath4@gmail.com>
2022-02-10 16:11:48 +0530

    Update string_reverse_a_string.go

--- LeetCode/Easy/string_reverse_a_string.go
+++ LeetCode/Easy/string_reverse_a_string.go
@@ -1,6 +1,22 @@
+package main
+
 import "fmt"
 
+/*
+URL: https://leetcode.com/problems/reverse-string/
+Status: Success
+Runtime: 28 ms, faster than 99.13% of Go online submissions for Reverse String.
+Memory Usage: 6.6 MB, less than 97.93% of Go online submissions for Reverse String.
+*/
+
+
 // strings are immutable and you cannot do str[i]
+func reverseString(s []byte)  {
+    strLen := len(s)
+    for i:=0; i<strLen/2;i++{
+        s[i],s[strLen-1-i] = s[strLen-1-i], s[i]
+    }
+}
 
 func main() {
 	str := "ABCD"
