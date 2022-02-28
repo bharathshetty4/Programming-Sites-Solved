7cce39f2937c5aaa90db2b83fa25c280d8c93d2a
Bharath Kumar <shettybharath4@gmail.com>
2022-02-28 09:33:20 +0530

    Update binaryTree.go

--- GeeksForGeeks/Go/binaryTree/binaryTree.go
+++ GeeksForGeeks/Go/binaryTree/binaryTree.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // tree node structure
 type Node struct {
 	data       int
@@ -70,17 +72,62 @@ func addNode(root *Node, parentVal, newNodeVal int, leftChild bool) {
 	}
 }
 
+func inOrder(node *Node) {
+	if node == nil {
+		return
+	}
+	inOrder(node.leftChild)
+	fmt.Printf("%d->", node.data)
+	inOrder(node.rightChild)
+}
+
+// keep on checking children sum for left and right child, until we hit the leaf.
+// If one of the sum found to be not equal, return false
+// https://www.geeksforgeeks.org/check-for-children-sum-property-in-a-binary-tree/
+func checkForChildreSum(node *Node) bool {
+	if node == nil || (node.leftChild == nil && node.rightChild == nil) {
+		return true
+	}
+	var l, r int
+	if node.leftChild != nil {
+		l = node.leftChild.data
+	}
+	if node.rightChild != nil {
+		r = node.rightChild.data
+	}
+	fmt.Printf("Node: %d, l:%d, r:%d\n", node.data, l, r)
+
+	if node.data != l+r {
+		return false
+	}
+	if node.leftChild != nil {
+		lChildCheck := checkForChildreSum(node.leftChild)
+		if !lChildCheck {
+			return false
+		}
+	}
+	if node.rightChild != nil {
+		rChildCheck := checkForChildreSum(node.rightChild)
+		if !rChildCheck {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 	// node structure
-	//                         10
+	//                         50
 	//                     20      30
-	//                   40 50    60 70
+	//                   10 10    10 20
 	// root node
-	root := newNode(10)
-	addNode(root, 10, 20, true)
-	addNode(root, 10, 30, false)
-	addNode(root, 20, 40, true)
-	addNode(root, 20, 50, false)
-	addNode(root, 30, 60, true)
-	addNode(root, 30, 70, false)
+	root := newNode(50)
+	addNode(root, 50, 20, true)
+	addNode(root, 50, 30, false)
+	addNode(root, 20, 10, true)
+	addNode(root, 20, 10, false)
+	addNode(root, 30, 10, true)
+	addNode(root, 30, 20, false)
+	// inOrder(root)
+	// fmt.Println(checkForChildreSum(root))
 }
