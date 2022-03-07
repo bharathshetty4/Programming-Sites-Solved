257a91ab85215129446e62ca839845f64352dbf8
Bharath Kumar <shettybharath4@gmail.com>
2022-03-07 19:46:41 +0530

    Update simple_int_ptr_by_ref_increment.go

--- Go_Samples/samples/simple_int_ptr_by_ref_increment.go
+++ Go_Samples/samples/simple_int_ptr_by_ref_increment.go
@@ -14,3 +14,19 @@ func main() {
 func incrInt(d *int) {
 	*d++
 }
+
+// ptr by reference for int array
+func inorderTraversal(root *TreeNode) []int {
+    var list = new([]int)
+    inorder(root,list)
+    return *list
+}
+
+func inorder(node *TreeNode, list * []int) {
+    if node == nil {
+        return
+    }
+    inorder(node.Left, list)
+    *list = append(*list,node.Val)
+    inorder(node.Right, list)
+}
